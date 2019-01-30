b7e9586196cde63ef2a4dd11b20be0ce37ad3c68
pospi <pospi@spadgos.com>
2019-01-30 19:14:25 +1000

    remove old systemctl service defs as they are in one neat place now

--- i3lock.sh
+++ i3lock.sh
@@ -2,36 +2,6 @@
 #
 # i3lock wrapper script for nice lockscreens
 #
-# Requires two systemd service wrappers to trigger:
-#
-# i3lock-suspend.service:
-#   [Unit]
-#   Description=i3lock on suspend
-#   Before=sleep.target
-#
-#   [Service]
-#   User=pospi
-#   Type=forking
-#   Environment=DISPLAY=:0
-#   ExecStart=/home/pospi/.config/i3/i3lock.sh suspend
-#
-#   [Install]
-#   WantedBy=sleep.target
-#
-# i3lock-resume.service:
-#   [Unit]
-#   Description=i3lock on resume
-#   After=suspend.target
-#
-#   [Service]
-#   User=pospi
-#   Type=forking
-#   Environment=DISPLAY=:0
-#   ExecStart=/home/pospi/.config/i3/i3lock.sh resume
-#
-#   [Install]
-#   WantedBy=suspend.target
-#
 # To execute the script under normal conditions, just run without arguments.
 #
 # @author:  pospi <pospi@spadgos.com>
